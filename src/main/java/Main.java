a5f7420648103e43f7b9f84ba29cfaaf3d398f0a
Martin Holloway <hollowaymartin@hotmail.com>
2020-11-25 18:57:29 +0000

    Completed C

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hello, World!");
         int i=0;
         for (i=1;i<=10;i++)System.out.println(i);
+        int rt=0;
+        for (i=30;i<=40;i++)rt+=i;
+        System.out.println(rt);
     }
 }
